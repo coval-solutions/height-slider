e42ce0d6a833ac6ccb1aee507750363019f9f29f
Dae Won Kim <dkim@homex.com>
2020-10-24 22:16:38 -0400

    Added example app and upgraded flutter_svg to latest (Android not working)

---
+++ example/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:height_slider/height_slider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(
+        title: 'Demo',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int height = 170;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: HeightSlider(
+          height: height,
+          onChange: (val) => setState(() => height = val),
+        ),
+      ),
+    );
+  }
+}
